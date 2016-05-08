37dab95519cb1aefc7666035f3dc97d29fd8e0fd
forclan <hxplian1992@gmail.com>
2016-05-08 23:50:13 +0800

    delete store.subsrcibe functions; add Chrome redux dev tools instead

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import {render} from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers/reducer';
 import middleWare from './middleWare/middleWare';
-let finalCreateStore = applyMiddleware(middleWare)(createStore);
-let store = finalCreateStore(reducer);
+// let finalCreateStore = applyMiddleware(middleWare)(createStore);
+// let store = finalCreateStore(reducer);
+
+let store = createStore(reducer, compose(
+  applyMiddleware(middleWare),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 let rootElemet = document.getElementById('root');
-console.log('state init', store.getState());
-store.subscribe(()=>{
-  console.log('state is', store.getState());
-})
+// console.log('state init', store.getState());
+// store.subscribe(()=>{
+  // console.log('state is', store.getState());
+// })
 
 render(
   <Provider store={store}>
